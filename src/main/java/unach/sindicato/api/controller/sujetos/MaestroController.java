90c4498d18bf25763397439216efb9e3d1061f91
kevincyndaquil <aleplantsvsz@gmail.com>
2024-05-05 20:57:51 -0600

    UDD Api v0.7

--- src/main/java/unach/sindicato/api/controller/sujetos/MaestroController.java
+++ src/main/java/unach/sindicato/api/controller/sujetos/MaestroController.java
@@ -2,12 +2,14 @@ package unach.sindicato.api.controller.sujetos;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import unach.sindicato.api.controller.AuthController;
 import unach.sindicato.api.controller.PersistenceController;
 import unach.sindicato.api.persistence.sujetos.Maestro;
 import unach.sindicato.api.service.sujetos.MaestroService;
+import unach.sindicato.api.utils.response.UddResponse;
 
 @RestController
 @RequestMapping("/maestros")
@@ -18,5 +20,11 @@ public class MaestroController implements PersistenceController<Maestro>, AuthCo
     @Override
     public @NonNull MaestroService service() {
         return service;
+    }
+
+    @Override
+    @Transactional
+    public UddResponse save(Maestro maestro) {
+        return register(maestro);
     }
 }
